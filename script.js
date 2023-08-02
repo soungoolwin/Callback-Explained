95a2ef9e0e8746523cfd9c62a1daac7c4a73add0
Soung Oo Lwin <soungoolwin275@gmail.com>
2023-08-02 23:51:10 +0700

    Callback Explained

---
+++ script.js
@@ -0,0 +1,30 @@
+// let driveCar = (carName) => {
+//   console.log(carName + " is driving");
+// };
+
+// driveCar("BMW");
+
+//This is Main Function
+// let fetchData = (callback) => {
+//   setTimeout(() => {
+//     const data = {
+//       name: "smt",
+//       id: 1,
+//     };
+//     callback(data);
+//   }, 2000);
+// };
+
+// // This is callback function
+
+// let processData = (data) => {
+//   console.log("Data Receieved:", data);
+// };
+
+// console.log("Before Fetching");
+// fetchData(processData);
+// console.log("After Fetching");
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number) => {});
